43cd3df198f4edc50b1ac86a5a17434cdab24931
TiNezlobinsky <nezlobinsky@gmail.com>
2024-08-29 15:33:59 +0200

    ECG 3D Tracker from the developing branch

--- examples/trackers/3D/ecg_3d_tracker.py
+++ examples/trackers/3D/ecg_3d_tracker.py
@@ -9,31 +9,40 @@ import numpy as np
 import finitewave as fw
 
 # number of nodes on the side
-n = 200
+n = 100
+m = 10
 
-tissue = fw.CardiacTissue3D([n, n, 5])
+tissue = fw.CardiacTissue3D([n, n, m])
 # create a mesh of cardiomyocytes (elems = 1):
-tissue.mesh = np.ones([n, n, 5], dtype="uint8")
+tissue.mesh = np.ones([n, n, m], dtype="uint8")
 # add empty nodes on the sides (elems = 0):
 tissue.add_boundaries()
 
-# don't forget to add the fibers array even if you have an anisotropic tissue:
-tissue.fibers = np.zeros([n, n, 5, 3])
+theta, alpha = 0.25*np.pi, 0.1*np.pi/4
+tissue.fibers = np.zeros((n, n, m, 3))
+tissue.fibers[:, :, :, 0] = np.cos(theta) * np.cos(alpha)
+tissue.fibers[:, :, :, 1] = np.cos(theta) * np.sin(alpha)
+tissue.fibers[:, :, :, 2] = np.sin(theta)
+# add numeric method stencil for weights computations
+tissue.stencil = fw.AsymmetricStencil3D()
+tissue.D_al = 1
+tissue.D_ac = tissue.D_al/9
 
 # create model object:
 aliev_panfilov = fw.AlievPanfilov3D()
-aliev_panfilov.dt = 0.01
-aliev_panfilov.dr = 0.25
-aliev_panfilov.t_max = 120
+aliev_panfilov.dt = 0.0015
+aliev_panfilov.dr = 0.1
+aliev_panfilov.t_max = 30
 
 # set up stimulation parameters:
 stim_sequence = fw.StimSequence()
-stim_sequence.add_stim(fw.StimVoltageCoord3D(10, 1, 0, n, 0, 3, 0, n))
-# stim_sequence.add_stim(StimVoltageCoord3D(31, 1, 0, 100, 0, n))
+stim_sequence.add_stim(fw.StimVoltageCoord3D(0, 1, 1, 5, 1, n-1, 1, m-1))
 
 tracker_sequence = fw.TrackerSequence()
 ecg_tracker = fw.ECG3DTracker()
-ecg_tracker.measure_points = np.array([[100, 100, 10]])
+ecg_tracker.measure_coords = np.array([[n//2, n//2, m+3],
+                                       [n//4, n//2, m+3],
+                                       [3*n//4, 3*n//4, m+3]])
 tracker_sequence.add_tracker(ecg_tracker)
 
 # add the tissue and the stim parameters to the model object:
@@ -43,5 +52,8 @@ aliev_panfilov.tracker_sequence = tracker_sequence
 
 aliev_panfilov.run()
 
-plt.plot(ecg_tracker.ecg[0])
+
+plt.figure()
+for y in ecg_tracker.ecg:
+    plt.plot(np.arange(y.shape[0]) * aliev_panfilov.dt * ecg_tracker.step, y)
 plt.show()
--- finitewave/cpuwave3D/tracker/ecg_3d_tracker.py
+++ finitewave/cpuwave3D/tracker/ecg_3d_tracker.py
@@ -1,34 +1,87 @@
 import os
 import numpy as np
 from numba import njit, prange
-from scipy.spatial import distance
+from scipy import spatial
 
 from finitewave.core.tracker.tracker import Tracker
 
 
+@njit(parallel=True)
+def measure(mesh, curr, coord):
+    n0 = coord.shape[0]
+    n1 = curr.shape[0]
+    n2 = curr.shape[1]
+    n3 = curr.shape[2]
+
+    ecg = np.zeros(n0)
+    for i in prange(n0 * n1 * n2 * n3):
+        i0 = i // (n1 * n2 * n3)
+        i1 = i % (n1 * n2 * n3) // (n2 * n3)
+        i2 = (i % (n1 * n2 * n3)) % (n2 * n3) // n3
+        i3 = (i % (n1 * n2 * n3)) % (n2 * n3) % n3
+        if mesh[i1, i2, i3] != 1:
+            continue
+        ecg[i0] += curr[i1, i2, i3] / ((coord[i0, 0] - i1)**2 +
+                                       (coord[i0, 1] - i2)**2 +
+                                       (coord[i0, 2] - i3)**2)
+    return ecg
+
+
 class ECG3DTracker(Tracker):
-    def __init__(self):
+    def __init__(self, memory_save=False):
         Tracker.__init__(self)
-        self.measure_points = np.array([[0, 0, 1]])
+        # self.radius = radius
+        self.measure_coords = np.array([[0, 0, 1]])
         self.ecg = np.ndarray
         self.step = 1
         self._index = 0
+        self.memory_save = memory_save
 
     def initialize(self, model):
         self.model = model
-        n = int(np.ceil(model.t_max / (self.step * model.dt)))
-        self.ecg = np.zeros((self.measure_points.shape[0], n))
+        n = self.measure_coords.shape[0]
+        m = int(np.ceil(model.t_max / (self.step * model.dt)))
+        self.ecg = np.zeros((n, m), dtype=model.npfloat)
+        self.tissue_coords = np.argwhere(model.cardiac_tissue.mesh == 1
+                                         ).astype(np.int32)
 
-        mesh = model.cardiac_tissue.mesh
-        self.tissue_points = np.where(mesh == 1)
+        if self.memory_save:
+            self.uni_voltage = self._uni_voltage_memory_save
+            return
 
-        tissue_points = np.argwhere(mesh == 1)
-        self.distances = distance.cdist(self.measure_points, tissue_points)
-        self.distances = self.distances**2
+        self.compute_distance()
+
+    def compute_distance(self):
+        self.distance = np.ones((self.measure_coords.shape[0],
+                                 self.tissue_coords.shape[0]),
+                                dtype=np.float16)
+
+        for i, point in enumerate(self.measure_coords):
+            self.distance[i, :] = np.sum((point - self.tissue_coords)**2,
+                                         axis=1).astype(np.float32)
+
+    def uni_voltage(self, current):
+        return np.sum(current[tuple(self.tissue_coords.T)] / self.distance,
+                      axis=1)
+
+    def _uni_voltage_memory_save(self, current):
+        return self.measure(current, self.measure_coords)
+
+    def measure(self, current, coords, batch_size=10):
+        ecg = []
+        split_inds = np.arange(coords.shape[0])[::batch_size][1:]
+        coords = np.split(coords, split_inds)
+        for coord in coords:
+            distance = spatial.distance.cdist(coord, self.tissue_coords)
+            ecg.append(np.sum(current[tuple(self.tissue_coords.T)] / distance ** 2,
+                              axis=1))
+        ecg = np.hstack(ecg)
+        return ecg
 
     def calc_ecg(self):
-        current = (self.model.u_new - self.model.u)[self.tissue_points]
-        return np.sum(current / self.distances, axis=1)
+        current = self.model.u_new - self.model.u
+        current[self.model.cardiac_tissue.mesh != 1] = 0
+        return self.uni_voltage(current) / self.model.dr
 
     def track(self):
         if self.model.step % self.step == 0:
