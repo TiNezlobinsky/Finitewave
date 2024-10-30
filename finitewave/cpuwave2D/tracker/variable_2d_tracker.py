77845c0bbd5e2924ad269359a5ec4d8cce1a97a0
bjoverst <bjorn.verstraeten@ugent.be>
2024-10-30 19:16:22 +0100

    fix griffe warnings

--- finitewave/cpuwave2D/tracker/variable_2d_tracker.py
+++ finitewave/cpuwave2D/tracker/variable_2d_tracker.py
@@ -1,4 +1,5 @@
 import os
+
 import numpy as np
 
 from finitewave.core.tracker.tracker import Tracker
@@ -6,10 +7,10 @@ from finitewave.core.tracker.tracker import Tracker
 
 class Variable2DTracker(Tracker):
     """
-    A tracker that records the values of specified variables from a 2D model 
+    A tracker that records the values of specified variables from a 2D model
     over time at a given grid point.
 
-    Parameters
+    Attributes
     ----------
     var_list : list of str
         List of variable names to be tracked.
@@ -19,12 +20,10 @@ class Variable2DTracker(Tracker):
         Directory name where the data will be saved. Default is "multi_vars".
     vars : dict
         Dictionary to store the tracked variables over time.
-
-    Attributes
-    ----------
     model : object
         The model object from which data is being tracked.
     """
+
     def __init__(self):
         Tracker.__init__(self)
         self.var_list = []
@@ -45,18 +44,20 @@ class Variable2DTracker(Tracker):
         """
         self.model = model
         t_max = self.model.t_max
-        dt    = self.model.dt
+        dt = self.model.dt
         for var_ in self.var_list:
-            self.vars[var_] = np.zeros(int(t_max/dt)+1)
+            self.vars[var_] = np.zeros(int(t_max / dt) + 1)
 
     def track(self):
         """
-        Updates the tracked variable values at the specified cell index for the current step.
+        Updates the tracked variable values at the specified cell index
+        for the current step.
         """
-        step  = self.model.step
+        step = self.model.step
         for var_ in self.var_list:
-            self.vars[var_][step] = self.model.__dict__[var_][self.cell_ind[0],
-                                                              self.cell_ind[1]]
+            self.vars[var_][step] = self.model.__dict__[var_][
+                self.cell_ind[0], self.cell_ind[1]
+            ]
 
     def write(self):
         """
