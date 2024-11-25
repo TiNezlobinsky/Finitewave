cb12003f4814a5d2c9506b51287033e7a7675968
Arstanbek Okenov <aokenov@ugent.be>
2024-11-25 19:25:52 +0100

    Remove unnessary lines from examples

--- examples/trackers/2D/act_time_2d_tracker.py
+++ examples/trackers/2D/act_time_2d_tracker.py
@@ -10,14 +10,18 @@ import finitewave as fw
 
 # number of nodes on the side
 n = 200
-
 tissue = fw.CardiacTissue2D([n, n])
-# create a mesh of cardiomyocytes (elems = 1):
-tissue.mesh = np.ones([n, n], dtype="uint8")
-# add empty nodes on the sides (elems = 0):
-tissue.add_boundaries()
-# don't forget to add the fibers array even if you have an anisotropic tissue:
-tissue.fibers = np.zeros([n, n, 2])
+
+# set up stimulation parameters:
+stim_sequence = fw.StimSequence()
+stim_sequence.add_stim(fw.StimVoltageCoord2D(0, 1, 0, 3, 0, n))
+
+# set up tracker parameters:
+tracker_sequence = fw.TrackerSequence()
+act_time_tracker = fw.ActivationTime2DTracker()
+act_time_tracker.threshold = 0.5
+act_time_tracker.step = 100  # calculate activation time every 100 * dt
+tracker_sequence.add_tracker(act_time_tracker)
 
 # create model object:
 aliev_panfilov = fw.AlievPanfilov2D()
@@ -25,18 +29,6 @@ aliev_panfilov = fw.AlievPanfilov2D()
 aliev_panfilov.dt = 0.01
 aliev_panfilov.dr = 0.25
 aliev_panfilov.t_max = 50
-
-# set up stimulation parameters:
-stim_sequence = fw.StimSequence()
-stim_sequence.add_stim(fw.StimVoltageCoord2D(0, 1, 0, 3, 0, n))
-
-tracker_sequence = fw.TrackerSequence()
-# add action potential tracker
-act_time_tracker = fw.ActivationTime2DTracker()
-act_time_tracker.threshold = 0.5
-act_time_tracker.step = 100  # calculate activation time every 100 * dt
-tracker_sequence.add_tracker(act_time_tracker)
-
 # add the tissue and the stim parameters to the model object:
 aliev_panfilov.cardiac_tissue = tissue
 aliev_panfilov.stim_sequence = stim_sequence
--- examples/trackers/2D/animation_2d_tracker.py
+++ examples/trackers/2D/animation_2d_tracker.py
@@ -9,30 +9,17 @@ import finitewave as fw
 # number of nodes on the side
 n = 100
 tissue = fw.CardiacTissue2D([n, n])
-# create a mesh of cardiomyocytes (elems = 1):
-tissue.mesh = np.ones([n, n], dtype="uint8")
-# add some fibrosis (elems = 2):
-tissue.mesh[np.random.random([n, n]) > 0.7] = 2
-# add empty nodes on the sides (elems = 0):
+# add some 30% of fibrosis
+tissue.mesh[np.random.random([n, n]) < 0.3] = 2
+# ensure that the boundaries are set correctly:
 tissue.add_boundaries()
-
-# don't forget to add the fibers array even if you have an anisotropic tissue:
-tissue.fibers = np.zeros([n, n, 2])
-
-# create model object:
-aliev_panfilov = fw.AlievPanfilov2D()
-
-# set up numerical parameters:
-aliev_panfilov.dt = 0.01
-aliev_panfilov.dr = 0.25
-aliev_panfilov.t_max = 50
 
 # set up stimulation parameters:
 stim_sequence = fw.StimSequence()
 stim_sequence.add_stim(fw.StimVoltageCoord2D(0, 1, 0, n, 0, 5))
 
+# set up tracker parameters:
 tracker_sequence = fw.TrackerSequence()
-# add action potential tracker
 animation_tracker = fw.Animation2DTracker()
 animation_tracker.variable_name = "u"  # Specify the variable to track
 animation_tracker.dir_name = "anim_data"
@@ -40,6 +27,12 @@ animation_tracker.step = 10
 animation_tracker.overwrite = True  # Remove existing files in dir_name
 tracker_sequence.add_tracker(animation_tracker)
 
+# create model object:
+aliev_panfilov = fw.AlievPanfilov2D()
+# set up numerical parameters:
+aliev_panfilov.dt = 0.01
+aliev_panfilov.dr = 0.25
+aliev_panfilov.t_max = 50
 # add the tissue and the stim parameters to the model object:
 aliev_panfilov.cardiac_tissue = tissue
 aliev_panfilov.stim_sequence = stim_sequence
